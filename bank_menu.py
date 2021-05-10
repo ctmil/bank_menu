667d3da35d38e8def4c7f774a2770ec92316edce
Ignacio Buioli <ibuioli@gmail.com>
2021-05-10 17:24:18 +0000

    Added XLS processing

--- bank_menu.py
+++ bank_menu.py
@@ -3,6 +3,9 @@ from datetime import date
 from odoo.exceptions import ValidationError
 from odoo.tools import float_is_zero
 import base64
+import xlrd
+import tempfile
+import binascii
 
 class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement'
@@ -18,6 +21,7 @@ class AccountBankStatement(models.Model):
     processed_file = fields.Boolean('Archivo procesado?')
     delimiter = fields.Char('Delimitador',default=',')
     total_processed = fields.Float('Total procesado',compute=_compute_total_processed)
+    xls_or_csv = fields.Selection(selection=[('csv','CSV'),('xls','XLS')],string='CSV o Excel',default='xls')
 
     def btn_import_file(self):
         self.ensure_one()
@@ -25,6 +29,32 @@ class AccountBankStatement(models.Model):
             raise ValidationError('Archivo ya procesado')
         if not self.statement_file:
             raise ValidationError('Debe ingresar el archivo')
+        if self.xls_or_csv == 'csv':
+            self.process_csv_file()
+        else:
+            self.process_xls_file()
+
+    def process_xls_file(self):
+        fp = tempfile.NamedTemporaryFile(suffix=".xlsx")
+        fp.write(binascii.a2b_base64(self.statement_file)) # self.xls_file is your binary field
+        fp.seek(0)
+        workbook = xlrd.open_workbook(fp.name)
+        sheet = workbook.sheet_by_index(0)
+        for row_no in range(sheet.nrows):
+            if row_no > 0:
+                vals = {
+                    'date': xlrd.xldate_as_datetime(sheet.cell_value(row_no,0),0),
+                    'name': sheet.cell_value(row_no,1),
+                    'payment_ref': sheet.cell_value(row_no,2),
+                    'amount': float(sheet.cell_value(row_no,3)),
+                    'statement_id': self.id,
+                    }
+                bank_line_id = self.env['account.bank.statement.line'].create(vals)
+
+
+
+
+    def process_csv_file(self):
         text_content = base64.b64decode(self.statement_file)
         text_lines = str(text_content).split('\\n')
         for i,text_line in enumerate(text_lines):
@@ -38,16 +68,20 @@ class AccountBankStatement(models.Model):
                 try:
                     amount = float(items[3].replace('\\r',''))
                 except:
-                    raise ValidationError('%s'%(items[3]))
+                    #raise ValidationError('%s'%(items[3]))
+                    continue
             else:
                 amount = 0
             vals = {
                     'date': items[0],
                     'name': items[2],
-                    'ref': items[1],
+                    'payment_ref': items[1],
                     'amount': amount,
                     'statement_id': self.id,
                     }
-            bank_line_id = self.env['account.bank.statement.line'].create(vals)
+            try:
+                bank_line_id = self.env['account.bank.statement.line'].create(vals)
+            except:
+                import pdb;pdb.set_trace()
         self.processed_file = True
 
